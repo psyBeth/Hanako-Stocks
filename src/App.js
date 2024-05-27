3b353d96ce6ba186a42447edac74b3d9648dbbb9
Betul <betulkoru06@gmail.com>
2024-05-27 12:20:02 +0300

    project structure built

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AppRouter from "./router/AppRouter";
+import { grey, blueGrey } from "@mui/material/colors";
+import { Provider } from "react-redux";
+import store from "./app/store";
+import { ToastContainer } from "react-toastify";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: grey["900"],
+      },
+      secondary: {
+        main: blueGrey["900"],
+      },
+    },
+  });
   return (
-    <div className="App">
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <AppRouter />
+        </Provider>
+        <ToastContainer />
+      </ThemeProvider>
+    </>
   );
-}
+};
 
 export default App;
