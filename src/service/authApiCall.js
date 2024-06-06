e27ba617320fb278208445fa9fbe0604880d01d8
Betul <betulkoru06@gmail.com>
2024-06-06 15:42:20 +0300

    auth func try catch export

--- src/service/authApiCall.js
+++ src/service/authApiCall.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
-const login = async () => {
-    const data = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/login`);
+export const login = async () => {
+    try {
+        const { data } = await axios.post(
+            `${process.env.REACT_APP_BASE_URL}/auth/login`,
+            userInfo
+        );
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
 };
