251382e6ca9856af5c6a9da966a1a08c71648a11
Betul <betulkoru06@gmail.com>
2024-06-09 13:02:32 +0300

    login navigating intro (cutomhook needed)

--- src/service/authApiCall.js
+++ src/service/authApiCall.js
@@ -1,14 +1,19 @@
 import axios from "axios";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
+import { useNavigate } from "react-router-dom";
 
 export const login = async (userInfo) => {
+    const navigate = useNavigate();
+
     try {
         const { data } = await axios.post(
             `${process.env.REACT_APP_BASE_URL}auth/login`,
             userInfo
         );
-        console.log(data);
-        // token shows on console
+        toastSuccessNotify("Login succeed.");
+        navigate("/stock");
     } catch (error) {
+        toastErrorNotify("Login failed.")
         console.log(error);
     };
 };
