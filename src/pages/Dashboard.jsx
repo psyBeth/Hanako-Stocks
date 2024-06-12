b7d758045461150c737cbfc7ccecc6b5c2cdcd49
Betul <betulkoru06@gmail.com>
2024-06-12 15:14:42 +0300

    dashboard updated (user auth)

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,9 +5,10 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { useSelector } from "react-redux";
 
 function Dashboard() {
-    const currentUser = true;
+    const { user } = useSelector((state) => state.auth);
 
     return (
         <Box sx={{ display: "flex" }}>
@@ -17,7 +18,7 @@ function Dashboard() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         HANAKO STOCKS
                     </Typography>
-                    {currentUser && <Button color="inherit">Logout</Button>}
+                    {user && <Button color="inherit">Logout</Button>}
                 </Toolbar>
             </AppBar>
         </Box>
