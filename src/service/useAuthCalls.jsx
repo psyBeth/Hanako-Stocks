f4e46607dac2bd870da039b93e7d8b2ac58bb920
Betul <betulkoru06@gmail.com>
2024-06-19 22:03:46 +0300

    feat: axios instance login ok

--- src/service/useAuthCalls.jsx
+++ src/service/useAuthCalls.jsx
@@ -1,8 +1,7 @@
-import axios from "axios";
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 import { useNavigate } from "react-router-dom";
 import { fetchFail, fetchStart, loginSuccess, logoutSuccess, registerSuccess } from "../features/authSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import useAxios from "./useAxios";
 
 const useAuthCalls = () => {
@@ -10,17 +9,14 @@ const useAuthCalls = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const { axiosWithToken, axiosPublic } = useAxios();
-    const { token } = useSelector((state) => state.auth);
+    // const { token } = useSelector((state) => state.auth);
 
     const login = async (userInfo) => {
 
         dispatch(fetchStart());
     
         try {
-            const { data } = await axios.post(
-                `${process.env.REACT_APP_BASE_URL}auth/login`,
-                userInfo
-            );
+            const { data } = await axiosPublic.post("auth/login/", userInfo);
             dispatch(loginSuccess(data));
             toastSuccessNotify("Login succeed.");
             navigate("/stock");
@@ -53,7 +49,6 @@ const useAuthCalls = () => {
             await axiosWithToken("auth/logout/");
             dispatch(logoutSuccess());
             toastSuccessNotify("Logged out successfully.");
-            navigate("/")
         } catch (error) {
             dispatch(fetchFail());
             toastErrorNotify("Logout failed.");
