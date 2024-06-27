41daa31e7ad51422064aa6d4741270f5f5bb9714
Betul <betulkoru06@gmail.com>
2024-06-27 19:56:32 +0300

    feat: menu list items component

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import StoreIcon from "@mui/icons-material/Store";
+import StarsIcon from "@mui/icons-material/Stars";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useNavigate } from "react-router-dom";
+
+const icons = [
+  {
+    icon: <SpaceDashboardIcon />,
+    title: "Dashboard",
+    url: "/stock/",
+  },
+  {
+    title: "Purchases",
+    icon: <ShoppingCartIcon />,
+    url: "/stock/purchases/",
+  },
+  {
+    title: "Sales",
+    icon: <AttachMoneyIcon />,
+    url: "/stock/sales/",
+  },
+  {
+    title: "Firms",
+    icon: <StoreIcon />,
+    url: "/stock/firms/",
+  },
+  {
+    title: "Brands",
+    icon: <StarsIcon />,
+    url: "/stock/brands/",
+  },
+  {
+    title: "Products",
+    icon: <InventoryIcon />,
+    url: "/stock/products/",
+  },
+]
 
 const MenuListItems = () => {
+  const navigate = useNavigate()
   return (
-    <div>MenuListItems</div>
+    <List>
+      {icons.map((item, index) => (
+        <ListItem
+          sx={{
+            color: "white",
+            "&:hover": { color: "#483D8B" },
+            "&:hover .MuiSvgIcon-root": { color: "#483D8B" },
+            "& .MuiSvgIcon-root": { color: "white" },
+          }}
+          key={index}
+          disablePadding
+          onClick={() => navigate(item.url)}
+        >
+          <ListItemButton>
+            <ListItemIcon sx={{ color: "white" }}>{item.icon}</ListItemIcon>
+            <ListItemText primary={item.title} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
   )
 }
 
-export default MenuListItems
+export default MenuListItems;
